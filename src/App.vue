05f7b6321e51f0a4ad56f65f9d3bde7b12767b7e
lyju777 <lyju777@hanmail.net>
2023-10-27 11:47:13 +0900

    proxy 설정 변경

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <main-page></main-page>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import mainPage from './components/MainPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    mainPage,
   },
 };
 </script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>main</div>
+</template>
+
+<script>
+import axios from 'axios';
+// import * as requestApi from '../api/requestAxios';
+
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+const URL = `${PROXY}/v1/papago/n2mt`;
+
+const instance = axios.create({
+  headers: {
+    'content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Naver-Client-Id': 'S0QZyjNjOWxp302OBVpC',
+    'X-Naver-Client-Secret': 'XuG1Hn0PJ6',
+  },
+});
+
+export default {
+  name: 'MainPage',
+
+  data() {
+    return {
+      data: {
+        source: 'ko',
+        target: 'en',
+        text: '파파고 테스트',
+      },
+    };
+  },
+
+  created() {
+    instance.post(URL, {
+      data: {
+        source: this.source,
+        target: this.target,
+        text: this.text,
+      },
+    });
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
